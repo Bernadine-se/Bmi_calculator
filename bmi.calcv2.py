e34ddacd0d32443225a7937c1038a27bf4812c2f
Bernadine-se <150375144+Bernadine-se@users.noreply.github.com>
2023-11-22 09:51:53 +0200

    Add files via upload

---
+++ bmi.calcv2.py
@@ -0,0 +1,21 @@
+print('welcome to the bmi calculator')
+weight = int(input('Please enter your weight in kg\n'))
+height = float(input('Please enter your height in cm\n'))
+
+bmi_calc = weight/height ** 2
+bmi =  (int(bmi_calc))
+
+if bmi < 18.5:
+    print (f'Your BMI is {bmi},you are underweight.')
+elif bmi < 25:
+         print (f'Your BMI is {bmi},you have a normal weight.')
+elif bmi < 30:
+          print (f'Your BMI is {bmi},you are slightly overweight.')
+elif bmi < 35:
+    print (f'Your BMI is {bmi},you are obese.')
+else:
+    print (f'Your BMI is {bmi},you are clinically obese.')
+
+
+
+
